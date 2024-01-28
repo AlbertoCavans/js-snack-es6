9961f9be7ec8b54e914dec52b8bc4da003a0cc18
AlbertoCavans <alberto96cavani@gmail.com>
2024-01-29 00:48:47 +0100

    Feat index - uppercase names

---
+++ Snack 2/JS/main.js
@@ -0,0 +1,45 @@
+/* ARRAY OF OBJECTS */
+const students = [
+  {
+    id: 213,
+    name: "Marco della Rovere",
+    grades: 78,
+  },
+  {
+    id: 110,
+    name: "Paola Cortellessa",
+    grades: 96,
+  },
+  {
+    id: 250,
+    name: "Andrea Mantegna",
+    grades: 48,
+  },
+  {
+    id: 145,
+    name: "Gaia Borromini",
+    grades: 74,
+  },
+  {
+    id: 196,
+    name: "Luigi Grimaldello",
+    grades: 68,
+  },
+  {
+    id: 102,
+    name: "Piero della Francesca",
+    grades: 50,
+  },
+  {
+    id: 120,
+    name: "Francesca da Polenta",
+    grades: 84,
+  },
+];
+
+/* UPPERCASE NAMES */
+let uppercaseStudents = students.map((students) => {
+  let nameUppercase = students.name.toUpperCase();
+  return nameUppercase;
+});
+console.log(uppercaseStudents);
